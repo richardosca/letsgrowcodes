3e409bf8a4807b9379944febdae801d6daf5357f
richardosca <116081637+richardosca@users.noreply.github.com>
2022-10-27 14:03:34 +0300

    Add files via upload

---
+++ compination.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main() {
+int sum1=10;
+int sum2=10;
+
+
+if(sum1>sum2){
+printf("sum1 is greater than sum2");
+}else if(sum1==sum2){
+printf("sum1 is equal to sum2");}
+else{printf("sum1 is less than sum2");}
+
+
+return 0;
+}
